7092fd545915c0c5f40ca9c5da61e782d2be451c
unknown <2769929M@student.gla.ac.uk>
2025-03-03 15:27:44 +0000

    initial

---
+++ src/main/java/com/apse_project/locharithm/LocharithmApplication.java
@@ -0,0 +1,13 @@
+package com.apse_project.locharithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocharithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocharithmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apse_project/locharithm/LocharithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apse_project.locharithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocharithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
