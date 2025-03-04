93628d1387962edb8dd7c8f2fc1a32400bd83394
Robert De Venny <2892269d@student.gla.ac.uk>
2025-03-04 13:07:18 +0000

    Added initial database config with liquidbase. Also created a proof-of-concept example controller.

---
+++ src/main/java/com/apse_project/locharithm/domain/Users.java
@@ -0,0 +1,25 @@
+package com.apse_project.locharithm.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Entity
+@Table(name="users")
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Users {
+
+    @Id
+    @Column(name="id")
+    private int id;
+
+    @Column(name="name")
+    private String name;
+}
