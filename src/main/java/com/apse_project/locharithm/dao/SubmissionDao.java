25e45520559ec4311a83579aed050651749ca2f1
Chris <2893430f@student.gla.ac.uk>
2025-03-05 15:41:26 +0000

    New Commit. Previous commit pushed no changes

--- src/main/java/com/apse_project/locharithm/dao/SubmissionDao.java
+++ src/main/java/com/apse_project/locharithm/dao/SubmissionDao.java
@@ -1,4 +1,11 @@
 package com.apse_project.locharithm.dao;
 
-public class SubmissionDao {
+import com.apse_project.locharithm.domain.Problem;
+import com.apse_project.locharithm.domain.Submission;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubmissionDao extends CrudRepository<Submission, Integer> {
+
 }
