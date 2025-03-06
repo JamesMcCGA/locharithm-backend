57e154d051133b07710f3d39a60f4998c20935a4
Robert De Venny <2892269d@student.gla.ac.uk>
2025-03-06 15:53:00 +0000

    Fix for CORS error

--- src/main/java/com/apse_project/locharithm/web/ProblemController.java
+++ src/main/java/com/apse_project/locharithm/web/ProblemController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping(path="api/problems/")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@CrossOrigin(origins="*")
 public class ProblemController {
 
     private final ProblemService problemService;
