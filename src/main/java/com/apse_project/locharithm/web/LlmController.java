33d0312cf8132818e73d7a78141601ec45242740
James <jamescarsonmccaffery@gmail.com>
2025-03-19 23:45:22 +0000

    better response parsing and using actual query

--- src/main/java/com/apse_project/locharithm/web/LlmController.java
+++ src/main/java/com/apse_project/locharithm/web/LlmController.java
@@ -3,6 +3,7 @@ package com.apse_project.locharithm.web;
 import com.apse_project.locharithm.client.OpenApiClient;
 import com.apse_project.locharithm.dtos.LlmRequestDto;
 import com.apse_project.locharithm.service.Judge0ApiService;
+import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,8 +26,12 @@ public class LlmController {
     }
 
     @PostMapping("/queryLlm")
-    public ResponseEntity<String> requestAi(@RequestBody LlmRequestDto llmRequestDto) throws IOException, InterruptedException {
+    public JsonObject requestAi(@RequestBody LlmRequestDto llmRequestDto) throws IOException, InterruptedException {
         String query = llmRequestDto.getPrompt();
-        return openApiClient.sendChatMessage(query);
+        ResponseEntity<String> apiResponse = openApiClient.sendChatMessage(query);
+        String reply = apiResponse.getBody();
+
+        JsonObject parsedResponse = openApiClient.parseResponse(reply);
+        return parsedResponse;
     }
 }
