797bd538df79516ae7ee7c0e23c5086443edb657
isym444 <samir.yep@gmail.com>
2025-03-20 14:56:52 +0000

    trying to bully AI into working

---
+++ src/main/java/com/apse_project/locharithm/dtos/ProblemGenerationResponse.java
@@ -0,0 +1,167 @@
+package com.apse_project.locharithm.dtos;
+
+public class ProblemGenerationResponse {
+    private String problemName;
+    private String inputConstraints;
+    private String inputFormat;
+    private String outputFormat;
+    private String sampleInput1;
+    private String sampleOutput1;
+    private String sampleInput2;
+    private String sampleOutput2;
+    private String testCaseInput1;
+    private String testCaseOutput1;
+    private String testCaseInput2;
+    private String testCaseOutput2;
+    private String testCaseInput3;
+    private String testCaseOutput3;
+    private String testCaseInput4;
+    private String testCaseOutput4;
+    private String testCaseInput5;
+    private String testCaseOutput5;
+
+    // Getters and setters
+    public String getProblemName() {
+        return problemName;
+    }
+
+    public void setProblemName(String problemName) {
+        this.problemName = problemName;
+    }
+
+    public String getInputConstraints() {
+        return inputConstraints;
+    }
+
+    public void setInputConstraints(String inputConstraints) {
+        this.inputConstraints = inputConstraints;
+    }
+
+    public String getInputFormat() {
+        return inputFormat;
+    }
+
+    public void setInputFormat(String inputFormat) {
+        this.inputFormat = inputFormat;
+    }
+
+    public String getOutputFormat() {
+        return outputFormat;
+    }
+
+    public void setOutputFormat(String outputFormat) {
+        this.outputFormat = outputFormat;
+    }
+
+    public String getSampleInput1() {
+        return sampleInput1;
+    }
+
+    public void setSampleInput1(String sampleInput1) {
+        this.sampleInput1 = sampleInput1;
+    }
+
+    public String getSampleOutput1() {
+        return sampleOutput1;
+    }
+
+    public void setSampleOutput1(String sampleOutput1) {
+        this.sampleOutput1 = sampleOutput1;
+    }
+
+    public String getSampleInput2() {
+        return sampleInput2;
+    }
+
+    public void setSampleInput2(String sampleInput2) {
+        this.sampleInput2 = sampleInput2;
+    }
+
+    public String getSampleOutput2() {
+        return sampleOutput2;
+    }
+
+    public void setSampleOutput2(String sampleOutput2) {
+        this.sampleOutput2 = sampleOutput2;
+    }
+
+    public String getTestCaseInput1() {
+        return testCaseInput1;
+    }
+
+    public void setTestCaseInput1(String testCaseInput1) {
+        this.testCaseInput1 = testCaseInput1;
+    }
+
+    public String getTestCaseOutput1() {
+        return testCaseOutput1;
+    }
+
+    public void setTestCaseOutput1(String testCaseOutput1) {
+        this.testCaseOutput1 = testCaseOutput1;
+    }
+
+    public String getTestCaseInput2() {
+        return testCaseInput2;
+    }
+
+    public void setTestCaseInput2(String testCaseInput2) {
+        this.testCaseInput2 = testCaseInput2;
+    }
+
+    public String getTestCaseOutput2() {
+        return testCaseOutput2;
+    }
+
+    public void setTestCaseOutput2(String testCaseOutput2) {
+        this.testCaseOutput2 = testCaseOutput2;
+    }
+
+    public String getTestCaseInput3() {
+        return testCaseInput3;
+    }
+
+    public void setTestCaseInput3(String testCaseInput3) {
+        this.testCaseInput3 = testCaseInput3;
+    }
+
+    public String getTestCaseOutput3() {
+        return testCaseOutput3;
+    }
+
+    public void setTestCaseOutput3(String testCaseOutput3) {
+        this.testCaseOutput3 = testCaseOutput3;
+    }
+
+    public String getTestCaseInput4() {
+        return testCaseInput4;
+    }
+
+    public void setTestCaseInput4(String testCaseInput4) {
+        this.testCaseInput4 = testCaseInput4;
+    }
+
+    public String getTestCaseOutput4() {
+        return testCaseOutput4;
+    }
+
+    public void setTestCaseOutput4(String testCaseOutput4) {
+        this.testCaseOutput4 = testCaseOutput4;
+    }
+
+    public String getTestCaseInput5() {
+        return testCaseInput5;
+    }
+
+    public void setTestCaseInput5(String testCaseInput5) {
+        this.testCaseInput5 = testCaseInput5;
+    }
+
+    public String getTestCaseOutput5() {
+        return testCaseOutput5;
+    }
+
+    public void setTestCaseOutput5(String testCaseOutput5) {
+        this.testCaseOutput5 = testCaseOutput5;
+    }
+}
