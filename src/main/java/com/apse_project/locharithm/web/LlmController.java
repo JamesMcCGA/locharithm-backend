0a020d42e83c7e80b3cb07fa1dc6858ae97d73e1
James <2769929M@student.gla.ac.uk>
2025-03-20 15:20:35 +0000

    PR feedback

--- src/main/java/com/apse_project/locharithm/web/LlmController.java
+++ src/main/java/com/apse_project/locharithm/web/LlmController.java
@@ -3,6 +3,7 @@ package com.apse_project.locharithm.web;
 import com.apse_project.locharithm.client.OpenApiClient;
 import com.apse_project.locharithm.dtos.LlmRequestDto;
 import com.apse_project.locharithm.service.Judge0ApiService;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -27,17 +28,23 @@ public class LlmController {
     }
 
     @PostMapping("/queryLlm")
-    public ResponseEntity<JsonObject> requestAi(@RequestBody LlmRequestDto llmRequestDto) {
+    public ResponseEntity<JsonArray> requestAi(@RequestBody LlmRequestDto llmRequestDto) {
         try {
             String query = llmRequestDto.getPrompt();
             ResponseEntity<String> apiResponse = openApiClient.sendChatMessage(query);
             String reply = apiResponse.getBody();
             JsonObject parsedResponse = openApiClient.parseResponse(reply);
-            return ResponseEntity.ok(parsedResponse);
+
+            // Wrap the JSON object inside an array
+            JsonArray array = new JsonArray();
+            array.add(parsedResponse);
+
+            return ResponseEntity.ok(array);
         } catch (IOException | InterruptedException e) {
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(null);
         }
     }
+
 }
