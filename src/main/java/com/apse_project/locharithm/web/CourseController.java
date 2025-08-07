da805a37a0012abccb50fce95816f4febfb9b8b9
James <2769929m@student.gla.ac.uk>
2025-08-07 21:17:46 +0100

    creating courseparticipant entities

--- src/main/java/com/apse_project/locharithm/web/CourseController.java
+++ src/main/java/com/apse_project/locharithm/web/CourseController.java
@@ -1,6 +1,7 @@
 package com.apse_project.locharithm.web;
 
 import com.apse_project.locharithm.domain.Course;
+import com.apse_project.locharithm.domain.CourseParticipant;
 import com.apse_project.locharithm.domain.User;
 import com.apse_project.locharithm.service.CourseParticipantService;
 import com.apse_project.locharithm.service.CourseService;
@@ -8,6 +9,7 @@ import com.apse_project.locharithm.service.TemporaryPasswordService;
 import com.apse_project.locharithm.service.UserService;
 import com.apse_project.locharithm.util.CourseParticipantsCsvParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -31,26 +33,30 @@ public class CourseController {
     @Autowired
     private CourseParticipantService courseParticipantService;
 
-    @Autowired
-    private TemporaryPasswordService temporaryPasswordService;
-
     @PostMapping("/uploadCourse")
-    public String uploadCourse(@RequestParam("file") MultipartFile file,
-                               @RequestParam("courseName")  String courseName,
-                               @RequestParam("instructorId") Integer instructorId) {
-
-
-        // get and persist the course participants from the csv...
-        List<User> courseParticipants = this.courseParticipantsCsvParser.parse(file);
-        List<User> persistedUsers = this.userService.createUsersFromList(courseParticipants);
+    public ResponseEntity<String> uploadCourse(@RequestParam("file") MultipartFile file,
+                                               @RequestParam("courseName")  String courseName,
+                                               @RequestParam("instructorId") Integer instructorId) {
 
         // create the course entity itself...
         Course course = new Course();
-        Optional<User> instructor = this.userService.getUserById(instructorId);
+        Optional<User> instructor = this.userService.getUserById(instructorId); // assumes the instructor is already an existing user when the course is created
         course.setCourseName(courseName);
         Course persistedCourse = this.courseService.createCourse(course, instructor.get());
 
-        System.out.println("Persisted course with ID: " + persistedCourse.getCourseId());
-        return "bahright";
+        // get and create the users from the csv...
+        List<User> courseStudents = this.courseParticipantsCsvParser.parse(file);
+        List<User> persistedUsers = this.userService.createUsersFromList(courseStudents);
+
+
+        // create the course entity linking the course and user
+        persistedUsers.forEach(persistedUser -> {
+            CourseParticipant courseParticipant = new CourseParticipant();
+            courseParticipant.setStudent(persistedUser);
+            courseParticipant.setCourse(persistedCourse);
+            this.courseParticipantService.createCourseParticipant(courseParticipant);
+        });
+
+        return ResponseEntity.ok("Course created successfully! ");
     }
 }
